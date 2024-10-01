de315f44ae22baa9c683c914ce278b3caf933e3b
Diversantos <divsrv@gmail.com>
2024-10-01 21:37:46 +0300

    Added tee.go and prepare log subsytem

--- docs/extra.md
+++ docs/extra.md
@@ -2,3 +2,4 @@
 
 * [Library](https://github.com/go-telegram-bot-api/telegram-bot-api)
 * [Emoji](https://apps.timwhitlock.info/emoji/tables/unicode)
+* [tee.go](https://gist.github.com/jerblack/4b98ba48ed3fb1d9f7544d2b1a1be287)
--- main.go
+++ main.go
@@ -11,13 +11,15 @@ import (
 	"time"
 	"os"
 	"encoding/json"
+	"io"
 	"io/ioutil"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 type Config struct {
-	ConfigDebug bool `json:"debug"`	
+	ConfigDebug bool `json:"debug"`
+	ConfigLogFile string `json:"logfile"`	
 }
 
 var (
@@ -47,6 +49,11 @@ func main() {
 	err = json.Unmarshal(file, &config)
 	if err != nil {
 		log.Fatal("Can't unmarshal config.json", err)
+	}
+
+	if config.ConfigLogFile != "none" {
+		fn := logOutput(config.ConfigLogFile)
+		defer fn()
 	}
 
 	// Get SECRET
@@ -96,6 +103,45 @@ func main() {
 		if err != nil {
 			log.Println("Some error with send: ", err)
 		}
+	}
+}
+
+
+func logOutput(logfile string) func() {
+	// open file read/write | create if not exist | clear file at open if exists
+	f, _ := os.OpenFile(logfile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+
+	// save existing stdout | MultiWriter writes to saved stdout and file
+//	out := os.Stdout
+	mw := io.MultiWriter(f)
+
+	// get pipe reader and writer | writes to pipe writer come out pipe reader
+	r, w, _ := os.Pipe()
+
+	// replace stdout,stderr with pipe writer | all writes to stdout, stderr will go through pipe instead (fmt.print, log)
+	os.Stdout = w
+	os.Stderr = w
+	
+	// writes with log.Print should also write to mw
+	log.SetOutput(mw)
+
+	//create channel to control exit | will block until all copies are finished
+	exit := make(chan bool)
+
+	go func() {
+		// copy all reads from pipe to multiwriter, which writes to stdout and file
+		_,_ = io.Copy(mw, r)
+		// when r or w is closed copy will finish and true will be sent to channel
+		exit <- true
+	}()
+
+	// function to be deferred in main until program exits
+	return func() {
+		// close writer then block on exit channel | this will let mw finish writing before the program exits
+		_ = w.Close()
+		<-exit
+		// close file after all writes have finished
+		_ = f.Close()
 	}
 }
 
